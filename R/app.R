64a2b7db542fee4cc1740c31ff60c6114f791e0b
stamats <matthias.kohl@stamats.de>
2023-04-26 11:02:30 +0200

    added button to save forestplot

--- R/app.R
+++ R/app.R
@@ -5,6 +5,7 @@ library(forestploter)
 library(metafor)
 library(shiny)
 library(shinythemes)
+library(ggplot2)
 
 ui <- fluidPage(
   theme = shinytheme("darkly"),
@@ -41,7 +42,8 @@ ui <- fluidPage(
                        label = "Select ENTREZ ID",
                        choices = NULL
         )
-      )
+      ),
+      downloadButton('savePlot', 'Save Forestplot (pdf)')
     ),
     mainPanel(
       plotOutput("plot", width = "100%", height = "720px")
@@ -71,8 +73,23 @@ server <- function(input, output, session) {
     if(input$ID == 3)
       gg <- forestplotFSHD(resData, entrezid = input$ENTREZ, called.in.app = TRUE)
     p.wh <- get_wh(plot = gg, unit = "in")
+    gg.plot <<- gg
     gg
   })
+  output$savePlot <- downloadHandler(
+    filename = function() { 
+      if(input$ID == 1)
+        filename <- paste("metaFSHD_", input$GeneName, ".pdf", sep = "")
+      if(input$ID == 2)
+        filename <- paste("metaFSHD_", input$ENSEMBL, ".pdf", sep = "")
+      if(input$ID == 3)
+        filename <- paste("metaFSHD_", input$ENTREZ, ".pdf", sep = "")
+      filename
+    },
+    content = function(file) {
+      ggsave(file, plot = gg.plot, width = 12, height = 8)
+    }
+  )
 }
 
 shinyApp(ui, server)
